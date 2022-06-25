29d2b25c8f328245f2cb3f251ed3b04a6bd82606
DavidPav123 <davidpavlicek123@gmail.com>
2022-06-24 21:58:42 -1000

    Uploading after finishing

--- Lists.py
+++ Lists.py
@@ -1,2 +1,24 @@
 if __name__ == '__main__':
     N = int(input())
+
+    arr = []
+
+    for i in range(0, N):
+        temp_arr = list(input().split())
+        
+        if temp_arr[0] == "insert":
+            arr.insert(int(temp_arr[1]), int(temp_arr[2]))
+        elif temp_arr[0] == "print":
+            print(arr)
+        elif temp_arr[0] == "remove":
+            arr.remove(int(temp_arr[1]))
+        elif temp_arr[0] == "append":
+            arr.append(int(temp_arr[1]))
+        elif temp_arr[0] == "sort":
+            arr.sort()
+        elif temp_arr[0] == "pop":
+            arr.pop()
+        elif temp_arr[0] == "reverse":
+            arr.reverse()
+
+        temp_arr.clear()
